3289c21855e24edfc1249803b902ea90dd92146f
zsy <zsy19980307@gmail.com>
2018-04-10 15:24:52 +0800

    remove redundant file, improve test

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 A temp project for red hat exam
 
 ### How to Build And Run
-1. Typing `mvn jetty:run` to start project directly
-2. Typing `mvn package` to generage War file, and deploying this war file to Tomcat/Jetty/JBoss
+1. Typing `mvn jetty:run` to run project directly
+2. Typing `mvn package` to generate War file, and deploying this war file to Tomcat/Jetty/JBoss
--- src/test/java/me/cizezsy/web/HelloWorldServletTest.java
+++ src/test/java/me/cizezsy/web/HelloWorldServletTest.java
@@ -1,42 +1,23 @@
 package me.cizezsy.web;
 
-
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
-public class HelloWorldServletTest extends Mockito {
-
-  @Mock
-  private HttpServletRequest request;
-  @Mock
-  private HttpServletResponse response;
-  @Mock
-  private RequestDispatcher dispatcher;
-
-
-  @Before
-  public void setUp() {
-    MockitoAnnotations.initMocks(this);
-    when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
-  }
+public class HelloWorldServletTest {
 
   @Test
-  public void testDoGet() throws ServletException, IOException {
-    new HelloWorldServlet().doGet(request, response);
-    ArgumentCaptor<String> da = ArgumentCaptor.forClass(String.class);
-    verify(request).getRequestDispatcher(da.capture());
-    Assert.assertEquals("/WEB-INF/jsp/hello.jsp", da.getValue());
+  public void testDoGet() {
+    WebDriver driver = new HtmlUnitDriver();
+    driver.get("http://localhost:8080/test/hello");
+    WebElement element = driver.findElement(By.tagName("p"));
+
+    assertThat(element.getText(), is("hello world"));
   }
 
 }
